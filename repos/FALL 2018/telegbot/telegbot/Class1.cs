6ee92154290a5004b0bd9896f57b08ba7ffaab36
Alex <alexey99979@yandex.ru>
2018-12-03 09:06:21 +0300

    bot

---
+++ repos/FALL 2018/telegbot/telegbot/Class1.cs
@@ -0,0 +1,137 @@
+﻿//using System;
+//using System.Threading;
+//using Telegram.Bot;
+//using System.Collections.Generic;
+//using System.Diagnostics;
+//using Telegram.Bot.Args;
+//using Telegram.Bot.Types.ReplyMarkups;
+
+//namespace Awesome
+//{
+//    class Program
+//    {
+
+//        static ITelegramBotClient botClient;
+//        static int count = 0;
+//        static Dictionary<int, string> phrases = new Dictionary<int, string>();
+//        static void Main()
+//        {
+//            botClient = new TelegramBotClient("636670621:AAE18_1_f0-FyG9kFEQNXY6T1SIIBdrtxv8");
+//            var me = botClient.GetMeAsync().Result;
+//            Console.WriteLine(
+//            $"Hello, World! I am user {me.Id} and my name is {me.FirstName}."
+//            );
+//            phrases.Add(1, "Выбери свой класс");
+//            phrases.Add(2, "Выбери своего первого противника");
+//            phrases.Add(3, "Выбери свою награду");
+//            phrases.Add(4, "Выбери свою принцессу");
+//            phrases.Add(5, "Твой дальнейшее действие?");
+//            botClient.OnMessage += Bot_OnMessage;
+//            botClient.StartReceiving();
+//            Thread.Sleep(int.MaxValue);
+//        }
+
+//        static async void Bot_OnMessage(object sender, MessageEventArgs e)
+//        {
+//            if (e.Message.Text == "старт") count++;
+//            var rkm = new ReplyKeyboardMarkup();
+//            if (count == 0)
+//            {
+//                await botClient.SendTextMessageAsync(
+//                chatId: e.Message.Chat,
+//                text: "Приветcтвую тебя, путник! Напиши \"старт\", чтобы начать игру.");
+//                return;
+//            }
+//            else if (count == 1)
+//            {
+//                rkm.Keyboard =
+//                new KeyboardButton[][]
+//                {
+//new KeyboardButton[]
+//{
+//new KeyboardButton("Воин."),
+//new KeyboardButton("Маг."),
+//new KeyboardButton("Убийца")
+//},
+//                };
+
+//                await botClient.SendTextMessageAsync(
+//                chatId: e.Message.Chat,
+//                text: phrases[count],
+//                replyMarkup: rkm);
+
+//                //await botClient.SendTextMessageAsync( 
+//                // chatId: e.Message.Chat, 
+//                // text: phrases[count], 
+//                // replyMarkup: new ReplyKeyboardRemove() { }); 
+//                count++;
+//                return;
+//            }
+//            else if (count == 2)
+//            {
+//                rkm.Keyboard =
+//                new KeyboardButton[][]
+//                {
+//new KeyboardButton[]
+//{
+//new KeyboardButton("Гоблин."),
+//new KeyboardButton("Скелет."),
+//new KeyboardButton("Морлок")
+//},
+//                };
+//                await botClient.SendTextMessageAsync(
+//                chatId: e.Message.Chat,
+//                text: phrases[count],
+//                replyMarkup: rkm);
+//            }
+//            else if (count == 3)
+//            {
+//                rkm.Keyboard =
+//                new KeyboardButton[][]
+//                {
+//new KeyboardButton[]
+//{
+//new KeyboardButton("Радужный плащ."),
+//new KeyboardButton("Платиновая корона."),
+//new KeyboardButton("Чугунная трость.")
+//},
+//                };
+//                await botClient.SendTextMessageAsync(
+//                chatId: e.Message.Chat,
+//                text: phrases[count],
+//                replyMarkup: rkm);
+//            }
+//            else if (count == 4)
+//            {
+//                rkm.Keyboard =
+//                new KeyboardButton[][]
+//                {
+//new KeyboardButton[]
+//{
+//new KeyboardButton("Фиона."),
+//new KeyboardButton("Керш."),
+//new KeyboardButton("Киркоров.")
+//},
+//                };
+//                await botClient.SendTextMessageAsync(
+//                chatId: e.Message.Chat,
+//                text: phrases[count],
+//                replyMarkup: rkm);
+//            }
+//            else if (count == 5)
+//            {
+//                rkm.Keyboard =
+//                new KeyboardButton[][]
+//                {
+//new KeyboardButton[]
+//{
+//new KeyboardButton("Спать."),
+//new KeyboardButton("Чистить зубы."),
+//new KeyboardButton("Писать код.")
+//},
+//                };
+//                await botClient.SendTextMessageAsync(
+//                chatId: e.Message.Chat,
+//                text: phrases[count],
+//replyMarkup: rkm);
+//            } 
---
+++ repos/FALL 2018/telegbot/telegbot/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("telegbot")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("telegbot")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("7104a110-7d12-4014-9fe3-b3e487e518c5")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номер сборки и номер редакции по умолчанию.
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
