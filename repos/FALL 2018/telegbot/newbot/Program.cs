fb26940ccdcb4343717266bc401ab00080539096
Alex <alexey99979@yandex.ru>
2018-12-16 17:04:57 +0300

    new bot

---
+++ repos/FALL 2018/telegbot/newbot/Program.cs
@@ -0,0 +1,68 @@
+﻿using com.LandonKey.SocksWebProxy;
+using com.LandonKey.SocksWebProxy.Proxy;
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using Telegram.Bot;
+using Telegram.Bot.Args;
+
+namespace Awesome
+{
+    class Program
+    {
+        static ITelegramBotClient botClient;
+
+        private static int GetNextFreePort()
+        {
+            var listener = new TcpListener(IPAddress.Loopback, 0);
+            listener.Start();
+            var port = ((IPEndPoint)listener.LocalEndpoint).Port;
+            listener.Stop();
+
+            return port;
+        }
+
+        static void Main()
+        {
+            //botClient = new TelegramBotClient(
+            //    "753674851:AAEJF2_jpSE8-t0Y8dgYyl_uHb_YnZuaprQ",
+            //    new SocksWebProxy(
+            //            new ProxyConfig(
+            //                IPAddress.Parse("127.0.0.1"),
+            //                GetNextFreePort(),
+            //                IPAddress.Parse("103.10.154.242"),
+            //                41501,
+            //                ProxyConfig.SocksVersion.Five
+            //                //"userid66n9",
+            //                //"pSnEA7M"),
+            //                )
+            //            //false
+            //            )
+            //            );
+
+            //var me = botClient.GetMeAsync().Result;
+            Console.WriteLine(
+              $"Hello, World! I am user {me.Id} and my name is {me.FirstName}."
+            );
+
+            botClient.OnMessage += Bot_OnMessage;
+            botClient.StartReceiving();
+            Thread.Sleep(int.MaxValue);
+        }
+
+        static async void Bot_OnMessage(object sender, MessageEventArgs e)
+        {
+            if (e.Message.Text != null)
+            {
+                Console.WriteLine($"Received a text message in chat {e.Message.Chat.Id}.");
+
+                await botClient.SendTextMessageAsync(
+                  chatId: e.Message.Chat,
+                  text: "You said:\n" + e.Message.Text
+                );
+            }
+        }
+    }
+}
+//753674851:AAEJF2_jpSE8-t0Y8dgYyl_uHb_YnZuaprQ
